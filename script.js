22998a47b4656eb5b66534f470c2e37b821d9c1e
Darmian359 <okeowojoshua70@gmail.com>
2026-03-24 15:53:08 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -59,3 +59,11 @@ document.getElementById(target).classList.add("active");
 });
 
 });
+
+
+  const toggle = document.getElementById("menu-toggle");
+const nav = document.querySelector("nav ul");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
